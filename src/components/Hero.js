cb80814e0af8bdeb32f5cf66c8a452fdf98a524f
iPeakajoo <peakajoo@gmail.com>
2025-03-04 07:59:41 +0700

    feat: update Hero and Navbar components for improved layout and styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,21 +2,27 @@ import Image from "next/image";
 const hero = () => {
   return (
     <>
-      <div className=" ">
-        <Image
-          src="/assets/images/banner.jpg"
-          layout="responsive"
-          width={1200}
-          height={800}
-          alt="Banner image"
-        />
-      </div>
-      <div className=" w-160">
-        <h1 className="text-6xl font-bold text-gray-600">Learn something new every day</h1>
-        <p className=" text-4xl text-gray-600 pt-[32px]">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        </p>
-        <button className=" text-white text-2xl  py-[8px] bg-gray-600 w-full mt-6">Start here</button>
+      <div className="flex items-center justify-center gap-[24px] ">
+        <div className=" ">
+          <Image
+            src="/assets/images/banner.jpg"
+            layout="responsive"
+            width={1200}
+            height={800}
+            alt="Banner image"
+          />
+        </div>
+        <div className=" w-160">
+          <h1 className="text-6xl font-bold text-gray-600">
+            Learn something new every day
+          </h1>
+          <p className=" text-4xl text-gray-600 pt-[32px]">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+          <button className=" text-white text-2xl  py-[8px] bg-gray-600 w-full mt-6">
+            Start here
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = ({ navigation = [] }) => {
   return (
     <header className="header">
      
-      <nav className="desktop-nav h-[64px] flex justify-between items-center mx-[24px] md:flex ">
+      <nav className="desktop-nav h-[64px] flex justify-between items-center mx-[24px]  md:flex hidden">
       <div className="flex items-center justify-center">
       <Image src={"/assets/images/ic-logo.svg"} width={50} height={50} alt="Logo" />
       <span className=" text-[32px] font-medium text-gray-600">COLMARAC</span>
       <span className=" text-[32px] font-normal text-gray-400">ACADEMY</span>
       </div>
       
-        <ul className="flex items-center justify-center gap-[16px] text-xl">
+        <ul className="flex items-center justify-center gap-[16px] text-xl text-gray-600">
           <li>
             <Link href="#">On Campus</Link>
           </li>
@@ -28,8 +28,8 @@ const Navbar = ({ navigation = [] }) => {
         </ul>
       
         </nav>
-      <nav className="mobile-nav p-2 md:hidden ">
-        <ul className="navbar-menu">
+      <nav className="mobile-nav p-4 md:hidden">
+        <ul className="navbar-menu flex items-center justify-between">
           {navigation.map((item, index) => (
             <li key={index}>
               <Link href={item.href}>
